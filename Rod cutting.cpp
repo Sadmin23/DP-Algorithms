3f137570e0553ebbd3158b3fdc7eef08537c817a
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2022-10-05 22:52:17 +0600

    testing recursion

--- Rod cutting.cpp
+++ Rod cutting.cpp
@@ -4,18 +4,21 @@ using namespace std;
 
 const int N = 10;
 
-int CUT_ROD_rec(int p[], int n)
+int p[N] = {1, 5, 8, 9, 10, 17, 20, 24, 30};
+int q;
+
+int CUT_ROD_rec(int n)
 {
     if (n == 0)
         return 0;
-    int q = INT_MIN;
+    q = INT_MIN;
 
-    for (int i = 0; i < n; i++)
-        q = max(q, p[i] + CUT_ROD_rec(p, n - i));
+    for (int i = 1; i <= n; i++)
+        q = max(q, p[i] + CUT_ROD_rec(n - i));
 
     return q;
 }
 int main()
 {
-    int p[N] = {1, 5, 8, 9, 10, 17, 20, 24, 30};
+    cout << CUT_ROD_rec(9) << "\n";
 }
