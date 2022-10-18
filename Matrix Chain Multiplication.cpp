5e005759309916edd3dec937e698510a78a33e16
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2022-10-18 21:07:23 +0600

    Matrix recursion

--- Matrix Chain Multiplication.cpp
+++ Matrix Chain Multiplication.cpp
@@ -2,41 +2,29 @@
 
 using namespace std;
 
-int m[100][100], s[100][100];
-
-#define INF INT_MAX
-
-void matrix_chain_order(vector<int> p)
+int matrix_recusrion(int p[], int i, int j)
 {
-    int n = p.size() - 1;
+    if (i == j)
+        return 0;
 
-    m[n][n];
-    s[n - 1][n - 1;]
+    int k, m = INT_MAX, q;
 
-        for (int i = 0; i < n; i++)
-            m[i][i] = 0;
-
-    for (int l = 1; l < n; l++)
+    for (k = i; k < j; k++)
     {
-        for (int i = 0; i < n - l - 1; i++)
-        {
-            int j = i + l - 1;
+        q = matrix_recusrion(p, i, k) + matrix_recusrion(p, k + 1, j) + p[i - 1] * p[k] * p[j];
 
-            m[i][j] = INF;
-
-            for (int k = i; k < j - 1; k++)
-            {
-                int q = m[i][k] + m[k + 1][j] + p[i] * p[k] * p[j];
-
-                if (q < m[i][j])
-                {
-                    m[i][j] = q;
-                    s[i][j] = k;
-                }
-            }
-        }
+        m = min(q, m);
     }
+
+    return m;
 }
 int main()
 {
+    int n = 4;
+
+    int p[4] = {10, 100, 5, 50};
+
+    cout << matrix_recusrion(p, 1, n - 1) << "\n";
+
+    return 0;
 }
